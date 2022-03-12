ee98276a4bb9f7f1cd55ef6fc35d6f2dd322cdb0
shystart <shystart@126.com>
2022-03-12 14:04:04 +0800

    First commit

---
+++ src/com/shy/net/test.java
@@ -0,0 +1,18 @@
+package com.shy.net;
+
+public class test {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!!");
+        System.out.println("Hello World!!");
+        System.out.println("Hello World!!");
+        System.out.println("Hello World!!");
+        System.out.println("Hello World!!");
+        System.out.println("Hello World!!");
+        System.out.println("Hello World!!");
+        System.out.println("Hello World!!");
+        System.out.println("Hello World!!");
+    }
+
+
+}
