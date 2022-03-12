e70565fa699aa88e1d0bd7b10053ccc8adcb628f
shystart <shystart@126.com>
2022-03-12 14:30:32 +0800

    push commit

---
+++ src/com/shy/net/test2.java
@@ -0,0 +1,16 @@
+package com.shy.net;
+
+public class test2 {
+
+    public static void main(String[] args) {
+        System.out.println("shy");
+        System.out.println("shy");
+        System.out.println("shy");
+        System.out.println("shy");
+        System.out.println("shy");
+        System.out.println("shy");
+        System.out.println("shy");
+        System.out.println("shy");
+    }
+
+}
